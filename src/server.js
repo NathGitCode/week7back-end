b0599b899832f3f589c6026a3679e0dc8f3ea1e6
Nathan Stringer <nathstringer@gmail.com>
2023-10-11 10:07:44 +0100

    stretch goals done

--- src/server.js
+++ src/server.js
@@ -32,33 +32,86 @@ const bookSchema = new mongoose.Schema({
 
 const Book = mongoose.model("book", bookSchema);
 
-app.get("/books", (request, response) => {
-  console.log(request.originalUrl);
-  const book = {
-    title: "lord of the rings",
-    author: "tolkein",
-    genre: "fantasy",
+// get request
+app.get("/books", async (request, response) => {
+  const allBooks = await Book.find();
+  const successResponse = {
+    message: "allbooks",
+    allBooks: allBooks,
   };
-  const successRespone = {
-    message: "success",
-    book: book,
-  };
-  response.send(successRespone);
+  response.send(successResponse);
 });
 
-app.post("/books", (request, response) => {
-  console.log(request.body);
-  const newBook = {
+// post a new book to db
+app.post("/books", async (request, response) => {
+  const newBook = await Book.create({
     title: request.body.title,
     author: request.body.author,
     genre: request.body.genre,
-  };
-  const successRespone = {
+  });
+  console.log(newBook);
+
+  const successResponse = {
     message: "success",
     newBook: newBook,
   };
 
-  response.send(successRespone);
+  response.send(successResponse);
+});
+
+// updates a single books author
+app.put("/books", async (request, response) => {
+  const query = { title: request.body.title };
+  const updateAuthor = await Book.findOneAndUpdate(query, {
+    author: request.body.author,
+  });
+
+  const successResponse = {
+    message: "updated",
+    updateAuthor: updateAuthor,
+  };
+  response.send(successResponse);
+});
+
+// deletes a single book
+app.delete("/books", async (request, response) => {
+  const deleteBook = await Book.deleteOne({
+    title: request.body.title,
+  });
+  const successResponse = {
+    message: "deleted",
+    deleteBook: deleteBook,
+  };
+  response.send(successResponse);
+});
+
+// delete all books
+app.patch("/books", async (request, response) => {
+  const deleteAllBooks = await Book.deleteMany({});
+  const successResponse = {
+    message: "deleted everything",
+    deleteAllBooks: deleteAllBooks,
+  };
+  response.send(successResponse);
+});
+
+// find single book by title
+app.get("/books/:title/", async (request, response) => {
+  const findByTitle = await Book.findOne({
+    title: request.params.title,
+  });
+  response.send(findByTitle);
+});
+
+// search on a book title route and update any field
+app.put("/books/:title/", async (request, response) => {
+  const query = { title: request.params.title };
+  const updateByTitle = await Book.findOneAndUpdate(query, {
+    title: request.body.title,
+    author: request.body.author,
+    genre: request.body.genre,
+  });
+  response.send(updateByTitle);
 });
 
 app.listen(5001, () => {
